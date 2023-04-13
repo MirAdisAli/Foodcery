13423d37deac3e53c747a51f85d610f1c6da6b47
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-04-13 08:17:53 +0600

    added

--- products/detail-product.php
+++ products/detail-product.php
@@ -47,6 +47,16 @@
 
     //var_dump($allRelatedProducts);
 
+    //validating cart products
+    if(isset($_SESSION['user_id'])){
+        $validate = $conn->query("SELECT *FROM cart WHERE pro_id='$id' AND user_id= '$_SESSION[user_id]'");
+        $validate->execute();
+    }
+
+
+
+
+
 
 ?>
 
@@ -93,28 +103,28 @@
                         <form method="POST" id="form-data">
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text"  name="pro_title" value="<?php echo $product->title; ?>" >
+                                <input class="form-control" type="hidden"  name="pro_title" value="<?php echo $product->title; ?>" >
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="pro_image" value="<?php echo $product->image; ?>" >
+                                <input class="form-control" type="hidden" name="pro_image" value="<?php echo $product->image; ?>" >
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="pro_price" value="<?php echo $product->price; ?>" >
+                                <input class="form-control" type="hidden" name="pro_price" value="<?php echo $product->price; ?>" >
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="user_id" value="<?php echo $_SESSION['user_id']; ?>" >
+                                <input class="form-control" type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>" >
                             </div>
                         </div>
 
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="pro_id" value="<?php echo $product->id; ?>" >
+                                <input class="form-control" type="hidden" name="pro_id" value="<?php echo $product->id; ?>" >
                             </div>
                         </div>
                         <div class="row">
@@ -123,10 +133,15 @@
                             </div>
                             <div class="col-sm-6"><span class="pt-1 d-inline-block">Pack (1000 gram)</span></div>
                         </div>
-
+                        <?php if($validate->rowCount()>0) : ?>
+                        <button name="submit" type="submit" class="btn-insert mt-3 btn btn-primary btn-lg" disabled>
+                            <i class="fa fa-shopping-basket"></i> Added to Cart
+                        </button>
+                        <?php else : ?>
                         <button name="submit" type="submit" class="btn-insert mt-3 btn btn-primary btn-lg">
                             <i class="fa fa-shopping-basket"></i> Add to Cart
                         </button>
+                        <?php endif; ?>
                     </form> 
                     </div>
                 </div>
